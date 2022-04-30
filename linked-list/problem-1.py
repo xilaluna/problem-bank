6c3e9e8efd9cb4275df6a485976cfb6057e7b243
xiluna <xilaluna2@gmail.com>
2022-04-29 21:35:46 -0700

    [add] prob 1 done

--- linked-list/problem-1.py
+++ linked-list/problem-1.py
@@ -1,1 +1,9 @@
 # Given a single-linked list, find the middle value in the list.
+
+def tortoiseAndHare(head):
+    slow = fast = head
+
+    while fast and fast.next:
+        slow = slow.next
+        fast = fast.next.next
+    return slow

6561cec0bbb7ca0a0249622e748a4334dab416e9
xiluna <xilaluna2@gmail.com>
2022-05-03 11:56:08 -0700

    [add] prob 2

---
+++ other-problems/problem-2.py
@@ -0,0 +1,27 @@
+# Given a binary search tree, convert it into a sorted doubly-linked list by
+# modifying the original tree nodes(do not create new nodes).
+
+def bstToDoubly(root):
+    if not root:
+        return
+
+    def dfs(node):
+        nonlocal head, tail
+        if not node:
+            return
+        dfs(node.left)
+        if tail:
+            tail.right = node
+            node.left = tail
+        else:
+            head = node
+        tail = node
+        dfs(node.right)
+
+    head, tail = None, None
+
+    dfs(root)
+    head.left = tail
+    tail.right = head
+
+    return head
---
+++ other-problems/problem-3.py
@@ -0,0 +1,3 @@
+# Let’s say a binary tree is "super balanced" if the difference
+# between the depths of any two leaf nodes is at most one.
+# Write a function to check if a binary tree is "super balanced".

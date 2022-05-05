a54c98e849f1b12e64ce136d54e515f8504caf15
xiluna <xilaluna2@gmail.com>
2022-05-04 23:08:12 -0700

    prob 6 done

--- trees/problem-6.py
+++ trees/problem-6.py
@@ -1,2 +1,19 @@
 # Given a binary search tree containing integers and a target integer,
 # come up with an efficient way to locate two nodes in the tree whose sum is equal to the target value.
+
+def two_sum_bst(root, k):
+    lookup = set()
+
+    def dfs(node):
+        if not node:
+            return False
+
+        y = k - node.val
+
+        if y in lookup:
+            return True
+        else:
+            lookup.add(node.val)
+        return dfs(node.left) or dfs(node.right)
+
+    return dfs(root)

0ae05d0bdd8d974377ebcac7ced02148cb76acec
xiluna <xilaluna2@gmail.com>
2022-05-04 22:23:53 -0700

    [add] prob 4 done

--- trees/problem-4.py
+++ trees/problem-4.py
@@ -1,1 +1,23 @@
 # Binary tree level order traversal
+
+import collections
+
+
+def level_order_traversal(root):
+    result = []
+    queue = collections.deque()
+    queue.append(root)
+
+    while queue:
+        queue_length = len(queue)
+        level = []
+        for i in range(queue_length):
+            node = queue.popleft()
+            if node:
+                level.append(node.val)
+                queue.append(node.left)
+                queue.append(node.right)
+        if level:
+            result.append(level)
+
+    return result

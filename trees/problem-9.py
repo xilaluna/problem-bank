459bc893b96b9f230db19a8b622b3387fa0ded1c
xiluna <xilaluna2@gmail.com>
2022-05-05 00:06:19 -0700

    [add] prob 9

--- trees/problem-9.py
+++ trees/problem-9.py
@@ -1,1 +1,13 @@
 # Given a binary tree, check whether it is a valid binary search tree (values in order).
+
+def validate_bst(root):
+    def validate(node, left, right):
+        if not node:
+            return True
+        if not (node.val < right and node.val > right):
+            return False
+
+        return validate(node.left, left, node.val) and validate(
+            node.right, node.val, right)
+
+    return validate(root, float("-inf"), float("-inf"))

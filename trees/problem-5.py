db288446f773f63ec795d9cace41b6197157aff0
xiluna <xilaluna2@gmail.com>
2022-05-04 23:08:07 -0700

    prob 5 done

--- trees/problem-5.py
+++ trees/problem-5.py
@@ -1,1 +1,15 @@
 # Given a binary search tree, reverse the order of its values by modifying the nodes’ links.
+
+def invert_bin_tree(root):
+    def invert(root):
+        if not root:
+            return None
+        temp = root.left
+        root.left = root.right
+        root.right = temp
+
+        invert(root.left)
+        invert(root.right)
+
+    invert(root)
+    return root

71d5a14f9311be06f34b6dd8e2ca45d74ef3a390
xiluna <xilaluna2@gmail.com>
2022-05-04 22:12:01 -0700

    [add] prob 2 done

--- trees/problem-2.py
+++ trees/problem-2.py
@@ -1,1 +1,14 @@
 # Binary Tree Pre-order-traversal
+
+def pre_order_traversal(root):
+    result = []
+
+    def traverse(root):
+        if not root:
+            return
+        result.append(root.val)
+        traverse(root.left)
+        traverse(root.right)
+
+    traverse(root)
+    return result

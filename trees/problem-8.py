851304d04e89312f69463921cf637baa8431a109
xiluna <xilaluna2@gmail.com>
2022-05-04 23:47:49 -0700

    [add] prob 8 done

--- trees/problem-8.py
+++ trees/problem-8.py
@@ -1,2 +1,13 @@
 # Let’s say a binary tree is "super balanced" if the difference between the depths of any two leaf nodes is at most one.
 # Write a function to check if a binary tree is "super balanced".
+
+def super_balance(root):
+    def dfs(root):
+        if not root:
+            return [True, 0]
+        left, right = dfs(root.left), dfs(root.right)
+        balanced = left[0] and right[0] and abs(left[1] - right[1]) <= 1
+
+        return [balanced, 1 + max(left[1], right[1])]
+
+    return dfs(root)[0]

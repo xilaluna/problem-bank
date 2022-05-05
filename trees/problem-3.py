7a2e2411637c2ce14fa98a3e2147ae6326d00719
xiluna <xilaluna2@gmail.com>
2022-05-04 22:12:08 -0700

    prob 3 done

--- trees/problem-3.py
+++ trees/problem-3.py
@@ -1,1 +1,14 @@
 # Binary tree post-order-traversal
+
+def post_order_traversal(root):
+    result = []
+
+    def traverse(root):
+        if not root:
+            return
+        traverse(root.left)
+        traverse(root.right)
+        result.append(root.val)
+
+    traverse(root)
+    return result

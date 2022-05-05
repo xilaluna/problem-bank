b3aff498ece759451ba59ce77f502ef9006f95e0
xiluna <xilaluna2@gmail.com>
2022-05-04 23:58:16 -0700

    [add] prob 7 done

--- trees/problem-7.py
+++ trees/problem-7.py
@@ -1,3 +1,23 @@
 # Given a binary tree containing numbers,
 # find the maximum sum path (the path that has the largest sum of node values).
 # The path may start and end at any node in the tree.
+
+def max_sum_path(root):
+    result = [root.val]
+
+    def dfs(root):
+        if not root:
+            return 0
+
+        left_max = dfs(root.left)
+        right_max = dfs(root.right)
+
+        left_max = max(left_max, 0)
+        right_max = max(right_max, 0)
+
+        result[0] = max(result[0], root.val + left_max + right_max)
+
+        return root.val + max(left_max, right_max)
+
+    dfs(root)
+    return result

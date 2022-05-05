ec85dee76102e72777d922a2d1bd532ea8190f85
xiluna <xilaluna2@gmail.com>
2022-05-04 22:00:53 -0700

    [add] prob 1 done

--- trees/problem-1.py
+++ trees/problem-1.py
@@ -1,1 +1,15 @@
 # Binary Tree In-order-traversal
+
+
+def inOrderTraversalRecursive(root):
+    result = []
+
+    def traverse(root):
+        if not root:
+            return
+        traverse(root.left)
+        result.append(root.val)
+        traverse(root.right)
+
+    traverse(root)
+    return result
